12a1d795f3e37397b7f749d8e26381c4d71273cc
Aryan Naik <naikaryan951@gmail.com>
2025-12-02 22:10:48 +0530

    Added Functionality to verify user

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom';
-
-
+import axios from 'axios';
+import { AppContent } from '../context/AppContext';
+import { toast } from 'react-toastify';
 
 const EmailVerify = () => {
 
+    axios.defaults.withCredentials = true;
     const navigate = useNavigate();
-
+    const {backendUrl, isLoggedIn, userData, getUserData} = useContext(AppContent);
     const inputRefs = React.useRef([]);
 
     const handleInput = (e, index) => {
@@ -32,10 +34,34 @@ const EmailVerify = () => {
         })
     }
 
+    const onSubmitHandler = async (e) => {
+        try {
+            e.preventDefault();
+            const otpArray = inputRefs.current.map(e => e.value);
+            const otp = otpArray.join('');
+
+            const {data} = await axios.post(backendUrl+'/api/auth/verify-account', {otp})
+
+            if(data.success){
+                toast.success(data.message);
+                getUserData();
+                navigate('/');
+            }else{
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error(error.message);
+        }
+    }
+
+    useEffect(() => {
+        isLoggedIn && userData && userData.isAccountVerified && navigate('/')
+    }, [isLoggedIn, userData]);
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-linear-to-br from-blue-200 to-purple-400">
         <img onClick={()=> navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
-        <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+        <form onSubmit={onSubmitHandler} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
             <h1 className='text-white text-2xl font-semibold text-center mb-4'>Email Verify OTP</h1>
             <p className='text-center mb-6 text-indigo-300'>Enter the six digit code sent to your email id</p>
             <div onPaste={handlePaste} className='flex justify-between mb-8'>
@@ -49,7 +75,6 @@ const EmailVerify = () => {
             </div>
             <button type='submit' className='w-full py-3 bg-linear-to-r from-indigo-500 to-indigo-900 text-white rounded-full'>
             Verify Email</button>
-
         </form>
     </div>
   )
