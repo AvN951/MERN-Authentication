df2422cad5623cb71f8a01fe549366a3ffe7c041
Aryan Naik <naikaryan951@gmail.com>
2025-12-02 23:08:28 +0530

    Added Reset Password using OTP function and added email templates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import {assets} from '../assets/assets'
+import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { AppContent } from '../context/AppContext';
 import axios from 'axios';
@@ -8,16 +8,16 @@ import { toast } from 'react-toastify';
 const Navbar = () => {
 
     const navigate = useNavigate();
-    const {userData, backendUrl, setUserData, setIsLoggedIn} = useContext(AppContent);
+    const { userData, backendUrl, setUserData, setIsLoggedIn } = useContext(AppContent);
 
-    const sendVerificationOtp=async ()=>{
+    const sendVerificationOtp = async () => {
         try {
             axios.defaults.withCredentials = true;
-            const {data} = await axios.post(backendUrl+'/api/auth/send-verify-otp');
-            if(data.success){
+            const { data } = await axios.post(backendUrl + '/api/auth/send-verify-otp');
+            if (data.success) {
                 navigate('/email-verify');
                 toast.success(data.message);
-            }else{
+            } else {
                 toast.error(data.message);
             }
         } catch (error) {
@@ -25,10 +25,10 @@ const Navbar = () => {
         }
     }
 
-    const logout = async ()=>{
+    const logout = async () => {
         try {
             axios.defaults.withCredentials = true;
-            const {data} = await axios.post(backendUrl+'/api/auth/logout');
+            const { data } = await axios.post(backendUrl + '/api/auth/logout');
             data.success && setIsLoggedIn(false);
             data.success && setUserData(false);
             navigate('/');
@@ -36,24 +36,26 @@ const Navbar = () => {
             toast.error(error.message);
         }
     }
-  return (
-    <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
-        <img src={assets.logo} alt="" className='w-28 sm:w-32'></img>
-        {userData ? 
-        <div className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>
-            {userData.name[0].toUpperCase()}
-            <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
-                <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
-                    {!userData.isAccountVerified && (<li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify Email</li>
-                    )}
-                    <li onClick={logout}className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
-                </ul>
-            </div>
-        </div> : 
-        <button onClick={()=>navigate('/login')} className='flex items-center gap-2 broder border-gray-500 rounded-full px-6 py-2 text-gray-800 hover: bg-gray-100 transition-all'>Login</button>
-        } 
-    </div>
-  )
+    return (
+        <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
+            <img src={assets.logo} alt="" className='w-28 sm:w-32'></img>
+            {userData ?
+                <div className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>
+                    {userData.name[0].toUpperCase()}
+                    <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
+                        <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
+                            {!userData.isAccountVerified && (<li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify Email</li>
+                            )}
+                            <li onClick={logout} className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
+                        </ul>
+                    </div>
+                </div> :
+                <button
+                    onClick={() => navigate('/login')}
+                    className="flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all">Login</button>
+            }
+        </div>
+    )
 }
 
 export default Navbar
